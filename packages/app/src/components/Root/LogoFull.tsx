dc154ef598f9e1493090fc13b8ef94a33e13cb90
“BMG93” <brenden.gammill@code.berlin>
2024-05-08 14:37:10 +0200

    UPD: updog

--- packages/app/src/components/Root/LogoFull.tsx
+++ packages/app/src/components/Root/LogoFull.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import MyCustomLogoFull from './logo/Tux.svg.png'
+import MyCustomLogoFull from './logo/dog.png'
 const LogoFull = () => {
   return <img src={MyCustomLogoFull} />;
 };
